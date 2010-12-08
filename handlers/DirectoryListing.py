0a0fb76a9fd33bf036a26bbd1250ae93fcaba9d3
Brian K. Jones <bkjones@gmail.com>
2010-12-08 09:20:35 -0500

    Cleaned up DirectoryListingHandler. Modularized the code for easier unit testing and easier reading, tightened up the logic for checking for a valid, existing path, etc.

--- handlers/DirectoryListing.py
+++ handlers/DirectoryListing.py
@@ -11,44 +11,81 @@ class DirectoryListingHandler(tornado.web.RequestHandler):
         our config's PackageDirs list, we return a 404.
 
         """
-        if not os.path.exists(directory):
-            raise tornado.web.HTTPError(404)
 
-        if not os.path.isdir(directory):
-            # the requested path is a file, not a dir.
-            # Make a best effort at figuring out what kind
-            # of package it is, and send it along.
-            ftype_enc = mimetypes.guess_type(directory)
+        # First things first: is the requested path under one of our 
+        # configured PackageDirs, and does it actually exist? 
+        valid_request = self.checkpath(directory)
 
-            if None not in ftype_enc:
-                ftype, encoding = ftype_enc
-                content_type = ftype
+
+        if valid_request:
+            # At this point, if the path didn't exist or wasn't under PackageDirs, we would've 
+            # already returned an HTTPError from checkpath.
+            if not os.path.isdir(directory):
+                # it's a file. 
+                self.return_file(directory)
             else:
-                content_type = 'application/octet-stream'
+                # it's a directory. Try to provide a basic directory listing.
+                root_dirs = [os.path.normpath(d) for d in self.application.settings['PackageDirs']]
+                logging.debug("PackageDirs: %s", root_dirs)
 
-            self.set_header('Content-Type', content_type)
-            self.set_header('Content-Disposition', 'attachment; filename=%s' % os.path.basename(directory))
+                # Get a directory listing
+                allentries = os.listdir(directory)
+                logging.debug("ALL ENTRIES IN %s: %s", directory, allentries)
 
-            f = open(directory, 'r').read()
-            self.write(f)
+                # Get the stat info for each thing in the listing.
+                dlist = [(x, os.lstat(os.path.normpath(os.path.join(directory,x)))) for x in allentries]
+                pardir = None
+
+                # if we're not requesting a base root_dir, there's a parent directory link. 
+                if os.path.normpath(directory) not in root_dirs:
+                    parent_directory = os.path.normpath(os.path.join(directory, '..'))
+                    parent_dir_stat = time.asctime(time.localtime(os.stat(parent_directory).st_mtime))
+                    pardir = [(parent_directory, parent_dir_stat)]
+
+                # filtering statinfo so you just have (name, mtime) for each directory entry.
+                output_entries = [(x, time.asctime(time.localtime(y.st_mtime))) for x,y in dlist]
+                logging.debug("OUTPUT ENTRIES: %s", output_entries)
+                logging.debug("DIRECTORY WE'RE LISTING: %s", directory)
+                page_title = "Listing of directory '%s'" % directory 
+                self.render("dlist.html", title=page_title, entries=output_entries, directory=directory, pardir=pardir)
+
+    def checkpath(self, requested_path):
+        """
+        Check that the requested path lives under one of the configured 
+        PackageDirs
+
+        """
+        logging.debug("Checking path: %s", requested_path)
+        valid = [os.path.normpath(requested_path).startswith(i) for i in self.application.settings['PackageDirs']]
+        if True in valid:
+            logging.debug("Path is valid: %s", valid)
+            # also check existence
+            if os.path.exists(requested_path):
+                logging.debug("Path exists - returning True")
+                return True
+            else:
+                raise tornado.web.HTTPError(404)
         else:
-            # it's a directory. Try to provide a basic directory listing.
-            # All paths need to exist under the basedir.
-            basedir = os.path.normpath('/Users/jonesy/Downloads')
-            allentries = os.listdir(directory)
-            logging.debug("ALL ENTRIES IN %s: %s", directory, allentries)
-            dlist = [(x, os.lstat(os.path.normpath(os.path.join(directory,x)))) for x in allentries]
-            pardir = None
-            if basedir != os.path.normpath(directory):
-                parent_directory = os.path.normpath(os.path.join(directory, '..'))
-                parent_dir_stat = time.asctime(time.localtime(os.stat(parent_directory).st_mtime))
-                pardir = [(parent_directory, parent_dir_stat)]
+            raise tornado.web.HTTPError(403)
+                
 
-            logging.debug("DLIST: %s", dlist)
+    def return_file(self, requested_file):
+        """
+        The requested path is a file, not a dir.  Make a best effort at 
+        figuring out what kind of file it is, and send it along.
 
-            # filtering statinfo so you just have (name, mtime) for each directory entry.
-            output_entries = [(x, time.asctime(time.localtime(y.st_mtime))) for x,y in dlist]
-            logging.debug("OUTPUT ENTRIES: %s", output_entries)
-            logging.debug("DIRECTORY WE'RE LISTING: %s", directory)
-            page_title = "Listing of directory '%s'" % directory
-            self.render("dlist.html", title=page_title, entries=output_entries, directory=directory, pardir=pardir)
+        """
+        ftype_enc = mimetypes.guess_type(requested_file)
+
+        if None not in ftype_enc:
+            ftype, encoding = ftype_enc
+            content_type = ftype
+        else:
+            content_type = 'application/octet-stream'
+
+        self.set_header('Content-Type', content_type)
+        self.set_header('Content-Disposition', 'attachment; filename=%s' % os.path.basename(requested_file))
+
+        f = open(requested_file, 'r').read()
+        self.write(f)
+
